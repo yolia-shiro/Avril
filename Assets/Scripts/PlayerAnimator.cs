60193c8742434380b8a91a92869bce6327c21a48
yolia-shiro <yolia17624550491@gmail.com>
2020-12-31 23:55:41 +0800

    Demo

---
+++ Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    private Animator anim;
+    private PlayerController playerController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        playerController = GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetFloat("speed", Mathf.Abs(playerController.xAxis));
+        anim.SetBool("isJump", playerController.isJump);
+        anim.SetBool("isGround", playerController.isGround);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Avril
+
+## 2020/12/31
+通过 2D Animation 和 2D IK 完成Avril的 Idle、Walk、Run、Jump 动画</br>
+绘制Avril的动画机(Idle、Walk、Run、Jump)
+实现Avril的移动、跳跃功能。
