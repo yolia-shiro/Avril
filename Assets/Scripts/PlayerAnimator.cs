6c6e761ef5c4000d1e3b5542e4e75bf5a153f1d1
yolia-shiro <yolia17624550491@gmail.com>
2021-01-02 22:11:38 +0800

    Demo

--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -6,12 +6,14 @@ public class PlayerAnimator : MonoBehaviour
 {
     private Animator anim;
     private PlayerController playerController;
+    private Rigidbody2D myRigidbody;
 
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
         playerController = GetComponent<PlayerController>();
+        myRigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -20,5 +22,6 @@ public class PlayerAnimator : MonoBehaviour
         anim.SetFloat("speed", Mathf.Abs(playerController.xAxis));
         anim.SetBool("isJump", playerController.isJump);
         anim.SetBool("isGround", playerController.isGround);
+        anim.SetFloat("velocity_y", myRigidbody.velocity.y);
     }
 }
--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 通过 2D Animation 和 2D IK 完成Avril的 Idle、Walk、Run、Jump 动画</br>
 绘制Avril的动画机(Idle、Walk、Run、Jump)</br>
 实现Avril的移动、跳跃功能。
+
+## 2020/01/02
+对Avril的绘制了法杖，并对一些细节进行了修改</br>
+绘制了Magic动画</br>
